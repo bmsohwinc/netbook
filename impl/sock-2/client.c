f947e71379183ddedbde4cabb7ed8b91514233e3
bmsohwinc <bharasou58@gmail.com>
2025-03-14 23:51:18 +0530

    Multi-client support with processes

--- impl/sock-2/client.c
+++ impl/sock-2/client.c
@@ -5,6 +5,9 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+
+#define BUFFER_SIZE 1024
+#define MAX_CLIENT_MSG_LEN 200
 #define PORT 8080
 
 
@@ -13,7 +16,7 @@ int main(int argc, char const* argv[])
     int status, valread, client_fd;
     struct sockaddr_in serv_addr;
     char* hello = "Hello from client";
-    char buffer[1024] = { 0 };
+    char buffer[BUFFER_SIZE] = { 0 };
     if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         printf("\n Socket creation error \n");
         return -1;
@@ -38,12 +41,28 @@ int main(int argc, char const* argv[])
         printf("\nConnection Failed \n");
         return -1;
     }
-    send(client_fd, hello, strlen(hello), 0);
-    printf("Hello message sent\n");
-    valread = read(client_fd, buffer,
-                   1024 - 1); // subtract 1 for the null
-                              // terminator at the end
-    printf("%s\n", buffer);
+
+    valread = read(client_fd, buffer, 1024 - 1);
+    printf("Server says: %s\n", buffer);
+
+
+    while (1) {
+        char client_msg[MAX_CLIENT_MSG_LEN];
+        printf("Your message: ");
+        fgets(client_msg, MAX_CLIENT_MSG_LEN, stdin);
+
+        send(client_fd, client_msg, strlen(client_msg), 0);
+
+        if (strcmp(client_msg, "exit\n") == 0) {
+            break;
+        }
+
+        memset(buffer, 0, BUFFER_SIZE);
+        valread = read(client_fd, buffer, 1024 - 1);
+        // subtract 1 for the null terminator at the end
+        printf("Server says: %s\n", buffer);
+
+    }
 
     // closing the connected socket
     close(client_fd);
---
+++ impl/sock-2/pollserver.c
@@ -0,0 +1,108 @@
+// Server side C program to demonstrate Socket
+// programming
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <asm-generic/socket.h>
+
+#define BUFFER_SIZE 1024
+#define MAX_CLIENTS 20
+#define MAX_MSG_LEN 200
+#define PORT 8080
+
+int main(int argc, char const* argv[])
+{
+    int server_fd, new_socket;
+    ssize_t valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    socklen_t addrlen = sizeof(address);
+    char buffer[1024] = { 0 };
+    char* hello = "Hello from server";
+
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Forcefully attaching socket to the port 8080
+    if (setsockopt(server_fd, SOL_SOCKET,
+                   SO_REUSEADDR | SO_REUSEPORT, &opt,
+                   sizeof(opt))) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    // Forcefully attaching socket to the port 8080
+    if (bind(server_fd, (struct sockaddr*)&address,
+             sizeof(address))
+        < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+
+    int client_idx = 0;
+
+    int clients[MAX_CLIENTS] = { 0 };
+
+    while (client_idx < MAX_CLIENTS) {
+        if ((new_socket
+            = accept(server_fd, (struct sockaddr*)&address,
+                    &addrlen))
+            < 0) {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+
+        clients[client_idx] = new_socket;
+        client_idx++;
+
+        int pid = fork();
+
+        if (pid < 0) {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        }
+
+        if (pid == 0) {
+
+            printf("Child speaks... I am %d. My parent is: %d\n", getpid(), getppid());
+
+            // welcome message
+            send(new_socket, hello, strlen(hello), 0);
+            
+            while (1) {
+                
+                memset(buffer, 0, BUFFER_SIZE);
+                valread = read(new_socket, buffer, 1024 - 1); 
+
+                printf("Client %d: %s\n", client_idx, buffer);
+
+                if (strcmp(buffer, "exit") == 0) {
+                    close(new_socket);
+                    break;
+                }
+
+                send(new_socket, buffer, strlen(buffer), 0);
+
+            }
+        }
+    }
+
+    // closing the listening socket
+    close(server_fd);
+    return 0;
+}
+
