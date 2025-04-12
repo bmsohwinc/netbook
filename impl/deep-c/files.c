9e8d7e7c84ab73de1f73ac0bf304d87fd7787b02
bmsohwinc <bharasou58@gmail.com>
2025-04-12 22:06:50 +0530

    Low level file i/o

--- impl/deep-c/files.c
+++ impl/deep-c/files.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
 
 void file1() {
     FILE *fp;
@@ -118,12 +122,188 @@ void file5() {
     fclose(fp_src);
 }
 
+void file6() {
+    struct employee {
+        char name[20];
+        int age;
+        double salary;
+    };
+
+    struct employee emps[] = {
+        {
+            "Jake", 25, 1000.00
+        },
+        {
+            "Alice", 30, 4000.50
+        },
+        {
+            "Hugh", 27, 3000.00
+        },
+        {
+            "Tim", 45, 10000.75
+        }
+    };
+
+    FILE* fp = fopen("emp.csv", "w");
+
+    if (fp == NULL) {
+        printf("Cannot open file.\n");
+        exit(1);
+    }
+
+    // fprintf(fp, "name,age,salary\n");
+    for (int i = 0; i < 4; i++) {
+        fprintf(fp, "%s %d %F\n", emps[i].name, emps[i].age, emps[i].salary);
+    }
+
+    fclose(fp);
+    printf("Write to CSV complete.\n");
+}
+
+void file7() {
+    struct employee {
+        char name[20];
+        int age;
+        double salary;
+    };
+
+    FILE* fp = fopen("emp.csv", "r");
+
+    if (fp == NULL) {
+        printf("Cannot open file.\n");
+        exit(1);
+    }
+
+    struct employee* emps[20];
+    int i = 0;
+    int n_rows = 0;
+    while (i < 20) {
+        struct employee *emp_row = (struct employee *) malloc(sizeof(struct employee));
+        int x = fscanf(fp, "%s %d %lF", emp_row -> name, &emp_row -> age, &emp_row -> salary);
+
+        if (x == EOF) {
+            break;
+        }
+
+        emps[i] = emp_row;
+        i++;
+        n_rows++;
+    }
+
+    for (int i = 0; i < n_rows; i++) {
+        printf("%d,%s,%d,%lF\n", i + 1, emps[i] -> name, emps[i] -> age, emps[i] -> salary);
+    }
+
+    fclose(fp);
+
+    printf("Read %d rows\n", n_rows);
+}
+
+void file8() {
+    struct employee {
+        char name[20];
+        int age;
+        double salary;
+    };
+
+    FILE* fp = fopen("emp_2.csv", "wb");
+
+    if (fp == NULL) {
+        printf("Cannot open file.\n");
+        exit(1);
+    }
+
+    struct employee emps[] = {
+        {
+            "Jake", 25, 1000.00
+        },
+        {
+            "Alice", 30, 4000.50
+        },
+        {
+            "Hugh", 27, 3000.00
+        },
+        {
+            "Tim", 45, 10000.75
+        }
+    };
+
+    for (int i = 0; i < 4; i++) {
+        fwrite(&emps[i], sizeof(emps[i]), 1, fp);
+    }
+
+    fclose(fp);
+    printf("Write to CSV complete.\n");
+}
+
+void file9() {
+    struct employee {
+        char name[20];
+        int age;
+        double salary;
+    };
+
+    FILE* fp = fopen("emp_2.csv", "rb");
+
+    if (fp == NULL) {
+        printf("Cannot open file.\n");
+        exit(1);
+    }
+
+    struct employee emp;
+
+    while(fread(&emp, sizeof(emp), 1, fp) == 1) {
+        printf("%s, %d, %lF\n", emp.name, emp.age, emp.salary);
+    }
+
+    fclose(fp);
+    printf("Read from CSV complete.\n");
+}
+
+void file10() {
+    int infile, outfile, bytes;
+    infile = open("fun1.c", O_RDONLY);
+    outfile = open("fun1_copy.c", O_CREAT | O_WRONLY, 0644);
+
+    if (infile == -1 || outfile == -1) {
+        printf("Cannot open files.\n");
+        exit(1);
+    }
+
+    int BF_SIZE = 512;
+    char buffer[BF_SIZE];
+
+    int tot_bytes = 0;
+    while (1) {
+        bytes = read(infile, buffer, BF_SIZE);
+
+        if (bytes == 0) {
+            break;
+        }
+
+        write(outfile, buffer, bytes);
+        tot_bytes += bytes;
+    }
+
+    printf("I: %d bytes copied.\n", tot_bytes);
+
+    close(infile);
+    close(outfile);
+}
+
+
+
 int main() {
     // file1();
     // file2();
     // file3();
     // file4();
-    file5();
+    // file5();
+    // file6();
+    // file7();
+    // file8();
+    // file9();
+    file10();
     return 0;
 }
 
